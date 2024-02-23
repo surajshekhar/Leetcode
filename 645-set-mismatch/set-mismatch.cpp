ac8737ac3cbf05dec190cfeee5c131b05f4f14c3
surajshekhar <123583162+surajshekhar@users.noreply.github.com>
2024-02-23 11:14:22 +0530

    Time: 81 ms (14.47%) | Memory: 35.3 MB (10.48%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> s;
+        set<int>st;
+        for(int i=0;i<nums.size();i++){
+            if(st.find(nums[i])!=st.end()){
+                s.push_back(nums[i]);
+            }
+            else{
+                st.insert(nums[i]);
+
+            }
+            
+        }
+        for(int i=1;i<=nums.size();i++){
+            if(st.find(i)==st.end()){
+               // s.push_back(i-1);
+                s.push_back(i);
+                break;
+            }
+
+        }
+   
+        return s;
+        
+
+        
+    }
+};
