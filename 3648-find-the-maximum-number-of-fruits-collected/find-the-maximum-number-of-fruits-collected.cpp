2955a841ecb589d0ecd0f9c791cfb15e61fd064e
Suraj Shekhar <123583162+surajshekhar@users.noreply.github.com>
2025-08-07 21:36:59 +0530

    Time: 117 ms (42.86%) | Memory: 156.1 MB (55.84%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,80 @@
+class Solution {
+private:
+    int n;
+    
+    // row = i, col = j
+    int dfs3(int row, int col, vector<vector<int>>& mat) {
+        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
+
+        if(memo[row][col] != -1){return memo[row][col];}
+        
+        int val = mat[row][col];
+        int res = 0;
+
+        if (row == col) {
+            res = max(res, dfs3(row + 1, col + 1, mat));
+        }
+        else if (row - 1 == col) {
+            res = max({res,
+                       dfs3(row + 1, col + 1, mat),
+                       dfs3(row, col + 1, mat)});
+        }
+        else {
+            res = max({res,
+                       dfs3(row + 1, col + 1, mat),
+                       dfs3(row, col + 1, mat),
+                       dfs3(row - 1, col + 1, mat)});
+        }
+        
+        return memo[row][col] = val + res;
+    }
+
+    int dfs2(int row, int col, vector<vector<int>>& mat) {
+        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
+
+        if(memo[row][col] != -1){return memo[row][col];}
+        
+        int val = mat[row][col];
+        int res = 0;
+
+        if (row == col) {
+            res = max(res, dfs2(row + 1, col + 1, mat));
+        }
+        else if (row == col - 1) {
+            res = max({res,
+                       dfs2(row + 1, col + 1, mat),
+                       dfs2(row + 1, col, mat)});
+        }
+        else {
+            res = max({res,
+                       dfs2(row + 1, col + 1, mat),
+                       dfs2(row + 1, col, mat),
+                       dfs2(row + 1, col - 1, mat)});
+        }
+        
+        return memo[row][col] = val + res;
+    }
+
+public:
+    vector<vector<int>> memo;
+    int maxCollectedFruits(vector<vector<int>>& mat) {
+        n = mat.size();
+        int total = 0;
+
+        memo = vector<vector<int>> (n, vector<int> (n, -1));
+
+        // child - 1
+        // he will eat all diagonal fruits, so set them to 0 
+        for (int i = 0; i < n; i++) {
+            total += mat[i][i];
+            mat[i][i] = 0;
+        }
+        
+        // child - 2
+        total += dfs3(n - 1, 0, mat);
+        // child - 3
+        total += dfs2(0, n - 1, mat);
+
+        return total;
+    }
+};
